6c7b2073e6bbf58b1d3b1f4bf9e2ea723e8fbcfd
brucelike <45371927+brucelike@users.noreply.github.com>
2021-03-26 00:21:43 -0400

    Update shape2fat.py

--- models/shape2fat.py
+++ models/shape2fat.py
@@ -1,1 +1,157 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 25 12:43:51 2021
 
+@author: bruce
+"""
+# -*- coding: utf-8 -*-
+"""
+Pixel wise cGAN to predict the body fat with body shape
+"""
+from numpy import load
+from numpy import zeros
+from numpy import ones
+from numpy.random import randint
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import Conv2D
+from tensorflow.keras.layers import Conv2DTranspose
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import BatchNormalization
+from matplotlib import pyplot
+#import ot
+#from sklearn.model_selection import train_test_split
+import csv
+import numpy as np
+# define the discriminator model
+def define_discriminator(image_shape):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # source image input
+    in_src_image = Input(shape=image_shape)
+    # target image input
+    in_target_image_s = Input(shape=image_shape)
+    in_target_image_v = Input(shape=image_shape)
+    # concatenate images channel-wise
+    merged = Concatenate()([in_src_image, in_target_image_s,in_target_image_v])
+    # C64
+    d = Conv2D(64, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(merged)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C128
+    d = Conv2D(128, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C256
+    d = Conv2D(256, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # C512
+    d = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # second last output layer
+    d = Conv2D(512, (4,4), padding='same', kernel_initializer=init)(d)
+    d = BatchNormalization()(d)
+    d = LeakyReLU(alpha=0.2)(d)
+    # patch output
+    d = Conv2D(1, (4,4), padding='same', kernel_initializer=init)(d)
+    patch_out = Activation('sigmoid')(d)
+    # define model
+    model = Model([in_src_image, in_target_image_s,in_target_image_v], patch_out)
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss='binary_crossentropy', optimizer=opt, loss_weights=[0.5])
+    return model
+
+# define an encoder block
+def define_encoder_block(layer_in, n_filters, batchnorm=True):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # add downsampling layer
+    g = Conv2D(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+    # conditionally add batch normalization
+    if batchnorm:
+        g = BatchNormalization()(g, training=True)
+    # leaky relu activation
+    g = LeakyReLU(alpha=0.2)(g)
+    return g
+
+# define a decoder block
+def decoder_block(layer_in, skip_in, n_filters, dropout=True):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # add upsampling layer
+    g = Conv2DTranspose(n_filters, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(layer_in)
+    # add batch normalization
+    g = BatchNormalization()(g, training=True)
+    # conditionally add dropout
+    if dropout:
+        g = Dropout(0.5)(g, training=True)
+    # merge with skip connection
+    g = Concatenate()([g, skip_in])
+    # relu activation
+    g = Activation('relu')(g)
+    return g
+
+# define the standalone generator model
+def define_generator(image_shape):
+    # weight initialization
+    init = RandomNormal(stddev=0.02)
+    # image input
+    in_image = Input(shape=image_shape)
+    # encoder model
+    e1 = define_encoder_block(in_image, 64, batchnorm=False)
+    e2 = define_encoder_block(e1, 128)
+    e3 = define_encoder_block(e2, 256)
+    e4 = define_encoder_block(e3, 512)
+    e5 = define_encoder_block(e4, 512)
+    e6 = define_encoder_block(e5, 512)
+    e7 = define_encoder_block(e6, 512)
+    # bottleneck, no batch norm and relu
+    b = Conv2D(512, (4,4), strides=(2,2), padding='same', kernel_initializer=init)(e7)
+    b = Activation('relu')(b)
+    # decoder model
+    ds1 = decoder_block(b, e7, 512)
+    ds2 = decoder_block(ds1, e6, 512)
+    ds3 = decoder_block(ds2, e5, 512)
+    ds4 = decoder_block(ds3, e4, 512, dropout=False)
+    ds5 = decoder_block(ds4, e3, 256, dropout=False)
+    ds6 = decoder_block(ds5, e2, 128, dropout=False)
+    ds7 = decoder_block(ds6, e1, 64, dropout=False)
+    gs = Conv2DTranspose(image_shape[2], (4,4), strides=(2,2), padding='same', kernel_initializer=init)(ds7)
+    dv1 = decoder_block(b, e7, 512)
+    dv2 = decoder_block(dv1, e6, 512)
+    dv3 = decoder_block(dv2, e5, 512)
+    dv4 = decoder_block(dv3, e4, 512, dropout=False)
+    dv5 = decoder_block(dv4, e3, 256, dropout=False)
+    dv6 = decoder_block(dv5, e2, 128, dropout=False)
+    dv7 = decoder_block(dv6, e1, 64, dropout=False)
+    # output
+    gv = Conv2DTranspose(image_shape[2], (4,4), strides=(2,2), padding='same', kernel_initializer=init)(dv7)
+    out_image_sat = Activation('tanh')(gs)
+    out_image_vat = Activation('tanh')(gv)
+    # define model
+    model = Model(in_image, [out_image_sat,out_image_vat])
+    return model
+
+# define the combined generator and discriminator model, for updating the generator
+def define_gan(g_model, d_model, image_shape):
+    # make weights in the discriminator not trainable
+    d_model.trainable = False
+    # define the source image
+    in_src = Input(shape=image_shape)
+    # connect the source image to the generator input
+    [gen_out_s, gen_out_v] = g_model(in_src)
+    # connect the source input and generator output to the discriminator input
+    dis_out = d_model([in_src, gen_out_s,gen_out_v])
+    # src image as input, generated image and classification output
+    model = Model(in_src, [dis_out, gen_out_s,gen_out_v])
+    # compile model
+    opt = Adam(lr=0.0002, beta_1=0.5)
+    model.compile(loss=['binary_crossentropy', 'mae'], optimizer=opt, loss_weights=[1,50,100])
+    return model
